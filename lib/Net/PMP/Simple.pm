6e3f9c0e9da78e40fa3f58414032b2596d830f91
Nathaniel Reindl <nrr@corvidae.org>
2012-03-17 06:03:52 -0500

    Assemble resource URLs and things.

--- lib/Net/PMP/Simple.pm
+++ lib/Net/PMP/Simple.pm
@@ -5,6 +5,7 @@ use strictures;
 
 use Moose;
 
+use DateTime;
 use HTTP::Request::Common;
 use LWP::UserAgent;
 use Try::Tiny;
@@ -50,6 +51,93 @@ sub __authentication_url {
 	my ($self, %options) = @_;
 	return unless $options{base_url};
 	return $options{base_url} . '/j_security_check';
+}
+
+sub __initialization_url {
+	my ($self, %options) = @_;
+	return unless $options{base_url};
+	return $options{base_url} . '/PassTrixMain.cc';
+}
+
+sub __resource_url {
+	my ($self, %options) = @_;
+	return unless $options{base_url};
+	return unless $options{account};
+	return unless $options{resource};
+	my %query_parameters = (
+		'RequestType' => 'PasswordRetrieved',
+		'SUBREQUEST' => 'XMLHTTP'
+		'account' => $options{account},
+		'resource' => $options{resource},
+	);
+	return $options{base_url} . '/jsp/xmlhttp/AjaxResponse.jsp'
+}
+
+sub __generate_password_url {
+	my ($self, %options) = @_;
+	return unless $options{base_url};
+	my $time;
+	my %query_parameters = (
+		'RequestType' => 'generate',
+		'Rule' => 'Standard',
+		'time' => $time,
+	);
+	return $options{base_url} . '/jsp/xmlhttp/AjaxResponse.jsp'
+}
+
+sub __change_password_url {
+	my ($self, %options) = @_;
+	return unless $options{base_url};
+	return unless $options{account};
+	return unless $options{resource};
+	my %query_parameters = (
+		'RequestType' => 'PasswordChange',
+		'hostName' => $host_name,
+		'accountName' => $account_name,
+		'defaultvalue_schar1' => $password,
+		'notes' = 'N/A',
+	);
+	return $options{base_url} . '/jsp/xmlhttp/AjaxResponse.jsp'
+}
+
+sub __create_resource_url {
+	my ($self, %options) = @_;
+	return unless $options{base_url};
+	return unless $options{account};
+	return unless $options{resource};
+	my %query_parameters = (
+		'SysName' => $resource,
+		'DNSName' => '',
+		'SysType' => 'Application',
+		'group' => 'Default Group',
+		'desc' => 'Managed App',
+		'dept' => '',
+		'resourceURL' => $resource_url,
+		'location' => $location,
+		'Rule' => 'Standard',
+		'resourcedefaultvalue_char1' => $application,
+		'tmp' => $resource,
+		'User1' => '',
+		'Pass1' => '',
+		'spassword' => '',
+		'isEnforcePolicy' => 'false',
+		'cpassword' => '',
+		'Note1' => '',
+		'Domain1' => '',
+		'UserAccount' => '',
+		'Password' => '',
+		'Notes' => '',
+		'Domain' => '',
+		'ConfirmPass' => '',
+		'Password' => $password,
+		'Domain' => '',
+		'Notes' => '',
+		'ConfirmPass' => $password,
+		'UserAccount' => $account,
+		'remotesync' => '0',
+		'remotemode' => 'ssh',
+	);
+	return $options{base_url} . '/jsp/xmlhttp/AjaxResponse.jsp'
 }
 
 =head2 __authentication_options
